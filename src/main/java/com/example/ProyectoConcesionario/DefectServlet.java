629797d7f2d598de12e0f18aa8cac61d14a0bc00
AlejaReyes0627 <aleja.reyes0627@hotmail.com>
2021-05-26 02:36:24 -0500

    si

--- src/main/java/com/example/ProyectoConcesionario/DefectServlet.java
+++ src/main/java/com/example/ProyectoConcesionario/DefectServlet.java
@@ -7,12 +7,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(urlPatterns = "/defects.do")
+@WebServlet(urlPatterns = "/defectos.do")
 public class DefectServlet  extends HttpServlet
 {
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
-        request.getRequestDispatcher("/WEB-INF/defects.jsp").forward(request, response);
+        request.getRequestDispatcher("/WEB-INF/defectos.jsp").forward(request, response);
     }
 
 }
